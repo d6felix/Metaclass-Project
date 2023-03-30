1fa4975904fd464567a60ca990e6661f8011ab27
d6felix <d6felix@gmail.com>
2023-03-30 16:41:59 +0300

    improved line under links

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -9,23 +9,17 @@ import { ReactComponent as UserIcon } from "./UserIcon.svg";
 export const Header = () => (
   <header className={classNames(styles.header)}>
     <StoreNameIcon className={classNames(styles.header__icon)} />
-    <Link
-      className={classNames(styles.header__link, styles.header__link_first)}
-      to="/products"
-    >
-      Products
+    <Link className={classNames(styles.header__link)} to="/products">
+      <p className={classNames(styles.header__text)}>Products</p>
+      <div className={classNames(styles.header__line)} />
     </Link>
-    <Link
-      className={classNames(styles.header__link, styles.header__link_second)}
-      to="/products"
-    >
-      Categories
+    <Link className={classNames(styles.header__link)} to="/products">
+      <p className={classNames(styles.header__text)}>Categories</p>
+      <div className={classNames(styles.header__line)} />
     </Link>
-    <Link
-      className={classNames(styles.header__link, styles.header__link_third)}
-      to="/products"
-    >
-      About Us
+    <Link className={classNames(styles.header__link)} to="/products">
+      <p className={classNames(styles.header__text)}>About Us</p>
+      <div className={classNames(styles.header__line)} />
     </Link>
     <BagIcon className={classNames(styles.header__cart)} />
     <UserIcon className={classNames(styles.header__user)} />
