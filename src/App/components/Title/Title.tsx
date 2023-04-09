0bb0e5f1185041f90deecbd55f56d9598a6d0912
d6felix <d6felix@gmail.com>
2023-04-09 10:37:30 +0300

    title simple css

--- src/App/components/Title/Title.tsx
+++ src/App/components/Title/Title.tsx
@@ -3,17 +3,15 @@ import classNames from "classnames";
 import styles from "./Title.module.scss";
 
 export type TitleProps = {
-  /** Заголовок карточки */
   title: string;
-  /** Подзаголовок карточки */
   subtitle: string;
 };
 
 export const Title: React.FC<TitleProps> = ({ title, subtitle }) => {
   return (
     <>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <h1 className={classNames(styles.title)}>{title}</h1>
+      <p className={classNames(styles.subtitle)}>{subtitle}</p>
     </>
   );
 };
