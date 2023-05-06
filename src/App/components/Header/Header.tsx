31c033cd98db12b3a2b1bbf0c29288cde40d6c18
d6felix <d6felix@gmail.com>
2023-05-06 19:10:20 +0300

    header adaptive

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 
 import { ReactComponent as BagIcon } from "./BagIcon.svg";
 import styles from "./Header.module.scss";
+import { ReactComponent as MenuIcon } from "./MenuIcon.svg";
 import { ReactComponent as StoreNameIcon } from "./StoreNameIcon.svg";
 import { ReactComponent as UserIcon } from "./UserIcon.svg";
 
 export const Header: React.FC = () => (
   <header className={classNames(styles.header)}>
-    <StoreNameIcon className={classNames(styles.header__icon)} />
+    <StoreNameIcon className={classNames(styles.header__logo)} />
     <span className={classNames(styles.header__container_link)}>
       <Link className={classNames(styles.header__link)} to="/products">
         <p className={classNames(styles.header__text)}>Products</p>
@@ -24,8 +25,15 @@ export const Header: React.FC = () => (
       </Link>
     </span>
     <div className={classNames(styles.header__container_icons)}>
-      <BagIcon className={classNames(styles.header__cart)} />
-      <UserIcon className={classNames(styles.header__user)} />
+      <BagIcon
+        className={classNames(styles.header__icon_cart, styles.header__icon)}
+      />
+      <UserIcon
+        className={classNames(styles.header__icon_user, styles.header__icon)}
+      />
+      <MenuIcon
+        className={classNames(styles.header__icon_menu, styles.header__icon)}
+      />
     </div>
   </header>
 );
